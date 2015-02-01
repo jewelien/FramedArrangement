8b9c4dc36928182d8ea492bb84ec8418d53c6803
Julien Guanzon <julienguanzon@gmail.com>
2015-01-31 18:10:39 -0700

    Add 4 views to view controller

--- FramedArrangement/ViewController.m
+++ FramedArrangement/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property (nonatomic) UIView *redView;
+@property (nonatomic) UIView *greenView;
+@property (nonatomic) UIView *blueView;
+@property (nonatomic) UIView *yellowView;
+
+
 @end
 
 @implementation ViewController
@@ -18,6 +24,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.redView = [UIView new];
+    self.redView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.redView];
+    
+    self.greenView = [UIView new];
+    self.greenView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:self.greenView];
+    
+    self.blueView = [UIView new];
+    self.blueView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:self.blueView];
+    
+    self.yellowView = [UIView new];
+    self.yellowView.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:self.yellowView];
+    
 }
 
 - (void)didReceiveMemoryWarning {
